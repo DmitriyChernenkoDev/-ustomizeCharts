9866d28bba5ede51ef424cf9d1537ca2c617c4b6
DmitriyChernenkoDev <46886824+DmitriyChernenkoDev@users.noreply.github.com>
2019-11-22 17:22:55 +0200

    Update BarLineChartBase.java

--- MPChartLib/src/main/java/com/github/mikephil/charting/charts/BarLineChartBase.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/charts/BarLineChartBase.java
@@ -476,7 +476,7 @@ public abstract class BarLineChartBase<T extends BarLineScatterCandleBubbleData<
 //                offsetRight += mAxisRight.getRequiredWidthSpace(mAxisRendererRight
 //                        .getPaintAxisLabels());
 //                 offsetRight += 140f;
-                offsetRight += Utils.convertDpToPixel(30f);
+                offsetRight += Utils.convertDpToPixel(70f);
             }
 
             if (mXAxis.isEnabled() && mXAxis.isDrawLabelsEnabled()) {
