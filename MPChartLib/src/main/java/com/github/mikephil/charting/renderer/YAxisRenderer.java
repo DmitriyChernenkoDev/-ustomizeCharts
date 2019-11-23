65c851e304d2303a7ba12c429c70b6019ab7229e
DmitriyChernenkoDev <46886824+DmitriyChernenkoDev@users.noreply.github.com>
2019-11-23 13:37:35 +0200

    Update YAxisRenderer.java

--- MPChartLib/src/main/java/com/github/mikephil/charting/renderer/YAxisRenderer.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/renderer/YAxisRenderer.java
@@ -411,7 +411,7 @@ public class YAxisRenderer extends AxisRenderer {
 
             c.drawPath(path, paintTriangle);
             mAxisLabelPaint.setTextSize(Utils.convertDpToPixel(12f));
-            c.drawText("" + limitIndicatorBlock, positionX +5, ourRange + offset, mAxisLabelPaint);
+            c.drawText("" + limitIndicatorBlock, positionX +5, ourRange + offset+3, mAxisLabelPaint);
             mAxisLabelPaint.setTextSize(mYAxis.getTextSize());
         }
     }
